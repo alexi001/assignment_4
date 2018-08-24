4624d143281ba356dc43e0f7f663e82cfe15af3c
alexi001 <harshshrm246@gmail.com>
2018-08-24 07:54:29 +0530

    1 .

---
+++ 1.java
@@ -0,0 +1,13 @@
+public class InvertedTriangle{
+    public static void main(String args[]) {
+        for (int i= 5; i >= 1; i--) { 
+            for (int j = 0; j < 5 - i; j++){
+                System.out.print(' ');
+			}
+            for (int j = (2 - i); j < (2 - i) + (2 * i - 1); j++){
+                System.out.print('*');
+			}
+            System.out.print('\n');
+        }
+	}
+}
